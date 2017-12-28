836ba7387e9a3a72605a5c0a9358498ef5f5a252
rahat <rahat.forja@gmail.com>
2017-12-28 09:00:16 -0600

    updated Battle migration

--- backend/migrations/20171227161539-create-battle.js
+++ backend/migrations/20171227161539-create-battle.js
@@ -2,32 +2,36 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('battles', {
-      battleid: {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.BIGINT
       },
       player1: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {model: 'users', key:'id'}
       },
       player2: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {model: 'users', key:'id'}
       },
       winner: {
-        type: Sequelize.SMALLINT
+        type: Sequelize.INTEGER
       },
       status: {
         type: Sequelize.CHAR(8)
       },
       battleground: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {model: 'battlegrounds', key:'id'}
       },
       score: {
         type: Sequelize.SMALLINT
       },
       tournament: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {model: 'tournaments', key:'id'}
       },
       createdAt: {
         allowNull: false,
