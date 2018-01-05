502378ad854943cb72b534cab62cc3576fa1f728
rahat <rahat.forja@gmail.com>
2018-01-05 06:09:59 -0600

    added get tb by id route

--- backend/routes/thumbbuddies.js
+++ backend/routes/thumbbuddies.js
@@ -4,28 +4,43 @@ const express = require('express'),
     router = express.Router(),
     passport = require('passport');
 
+//const Models = require('../models')
 const Thumbbuddy = require('../models').thumbbuddy;
 const User = require('../models').user;
+const Species = require('../models').species;
 
 // get thumbbuddy by id
-router.get('/:id', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+router.get('/get/:id', passport.authenticate('jwt', { session: false }), (req, res, next) => {
     //TODO
+    let id = req.query.param1;
+    Thumbbuddy.findById(id)
+        .then(thumbbuddy => {
+            res.send({ success: true, tb: thumbbuddy });
+        })
+        .catch(err => {
+            handleErr(err);
+            res.send({ success: false, msg: err.message });
+        })
 })
 
 // search thumbbuddy
 // put thumbbuddy //not for public
-router.post('/create', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+router.post('/create', passport.authenticate('jwt', { session: false }), (req, res, next) => {
 
 })
 
 // How is this admin role addressed - admin user?
 // update thumbbuddy
-router.post('/update/:id', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+router.post('/update/:id', passport.authenticate('jwt', { session: false }), (req, res, next) => {
     //TODO
 })
 
 // assign thumbbuddy to new owner
-router.post('/assign', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+router.post('/assign', passport.authenticate('jwt', { session: false }), (req, res, next) => {
     //TODO
 })
 
+//TODO - actually handle the error
+function handleErr(err) {
+    console.log('Error caught: ' + err);
+}
