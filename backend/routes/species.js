f367315cd98d707c5b95efad7839ae289fa3cbc2
rahat <rahat.forja@gmail.com>
2018-01-09 06:43:25 -0600

    added species routes

---
+++ backend/routes/species.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+const Species = require('../models').Species
+
+
+
+router.get('/get', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    Species.findAll()
+        .then(species => res.send({ success: true, species: species }))
+        .catch(err => res.send({ success: false, msg: err.message }))
+})
+
+router.get('/get/:id', passport.authenticate({ session: false }), (req, res, next) => {
+    let id = req.query.param1
+    Species.findOne({ where: { id: id } })
+        .then(species => res.send({ success: false, species: species }))
+        .catch(err => res.send({ success: true, msg: err.message }))
+})
+
+// router.get('/search/:param', passport.authenticate({session: false}), (req, res, next) => {
+//     let param = req.query.param1
+//     Species.findAll({where: {species: param}})
+// })
+
+router.post('/create', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    Species.create({
+        species: req.body.name,
+        description: req.body.description
+    })
+        .then(species => res.send({ success: true, species: species }))
+        .catch(err => res.send({ success: false, msg: err.message }))
+})
+
